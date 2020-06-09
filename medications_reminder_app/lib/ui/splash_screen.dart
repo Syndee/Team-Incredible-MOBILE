466f95b30d208a0e82566c3f1341c550a38143fd
Syndee <Cynthiaineh02@gmail.com>
2020-06-09 16:43:28 +0100

    splash screen

--- medications_reminder_app/lib/ui/splash_screen.dart
+++ medications_reminder_app/lib/ui/splash_screen.dart
@@ -1,21 +1,68 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 
 //Note that the colors are #40b26d(main colour) and sub colour #fdfcff
 //! Colours have now been included in the app_theme.dart file so you can use Theme.of(context).whatever_color you like
 //I already added the google fonts package, use poppins
 //I'M COUNTING ON YOU!!!
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+        Duration(seconds: 5),
+            () => Navigator.pushReplacementNamed(context, '/home'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: Center(
-          child: Text('Splash Screen')
+      backgroundColor: Colors.grey[50],
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  child:
+                  Center(
+                    child: Image.asset('images/MedBuzz.png', width: MediaQuery.of(context).size.width*.9,),
+                  )
+
+              ),
+
+              Expanded(
+                flex: 1,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                      ),
+                      Text("Get Started", style: TextStyle(color: Colors.grey[800], fontSize: 18.0, fontWeight: FontWeight.bold),
+                      )
+                    ]
+                ),
+              )
+            ],
+          ),
+
+
+        ],
       ),
     );
   }
